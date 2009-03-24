ebaf9426219e26a645833a00191b7548c06ffbb5
sensomedia <sensomedia@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-03-24 15:01:26 +0000

    wording
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/mooslide/trunk@18264 735d13b6-9817-0410-8766-e36946ffe9aa

--- pi1/class.tx_mooslide_pi1.php
+++ pi1/class.tx_mooslide_pi1.php
@@ -2,7 +2,7 @@
 /***************************************************************
 *  Copyright notice
 *
-*  (c) 2008 senomedia <>
+*  (c) 2009 senomedia <info@sensomedia.de>
 *  All rights reserved
 *
 *  This script is part of the TYPO3 project. The TYPO3 project is
